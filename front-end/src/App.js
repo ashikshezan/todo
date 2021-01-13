1134278547d9daf2de2810e710a5246bdebe884d
Your Name <you@example.com>
2021-01-13 23:54:13 +0600

    initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div className={"m-10 h-16 w-16 bg-gray-500"}></div>
+      <div className={"m-10 h-16 w-16 bg-gray-900 rounded-full"}></div>
+    </div>
+  );
+}
+export default App;
