3c86c897ac4c9b6737fa78dc5e72493565d36c65
ashikshezan <ashikshezan@gmail.com>
2021-01-16 17:29:52 +0600

    testing out

---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table className="min-w-full divide-y divide-white">
+                            <thead className="bg-gray-50">
+                            </thead>
+                            <tbody className="bg-indigo-700">
+                                <tr>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                        <div className="flex items-center">
+                                            <div className="flex-shrink-0 h-16 w-16">
+                                                <img className="h-16 w-16 rounded-full border" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt />
+                                            </div>
+                                            <div className="ml-4">
+                                                <div className="text-2xl text-white font-light leading-normal">
+                                                    Hello,<br />Louise!
+                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-white text-6xl font-thin">
+                                        13
+            <div className="text-sm inline-block leading-normal"> <strong>Friday</strong> <br />December 4</div>
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap text-left text-sm text-white font-bold leading-normal">
+                                        Events Today
+            <ul className="font-thin text-xs py-2">
+                                            <li>12:00 Title of Event</li>
+                                            <li>12:50 Title of Event</li>
+                                            <li>12:30 Title of Event</li>
+                                        </ul>
+                                    </td>
+                                </tr>
+                                {/* More rows... */}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default Navbar;
---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,89 @@
+import React, { useReducer, useContext, useState, useEffect } from 'react';
+import Task from '../components/Task'
+
+
+const API = 'http://127.0.0.1:8080/api'
+
+
+const initialState = {
+    loading: true,
+    error: '',
+    taksList: [],
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "FETCH_SUCCESS":
+            return {
+                loading: false,
+                erorr: '',
+                taksList: action.payload
+            }
+        case "ADD-TASK":
+            return {
+                ...state,
+                taksList: [...state.taksList, { id: Date.now(), title: action.payload }]
+            }
+
+        case "REMOVE-TASK":
+            const temp = state.taksList
+            console.log('debugging temp:------', temp);
+            return {
+                ...state,
+                taksList: temp.filter(item => item.id !== action.payload)
+            }
+        default:
+            return state
+    }
+}
+
+const Context = React.createContext();
+
+
+const Home = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const [inputValue, setInputValue] = useState('')
+
+    useEffect(() => {
+        fetch(API)
+            .then(resp => resp.json())
+            .then(resp => dispatch({ type: 'FETCH_SUCCESS', payload: resp }))
+            .catch(er => console.log('ERROR CONSOLE', er))
+    }, [])
+
+    return (
+        < Context.Provider value={dispatch}>
+            <div className="w-screen mt-20 grid place-items-center">
+                <div className="w-2/3 p-8 shadow-lg">
+                    <h1 className="m-10">TODO LIST</h1>
+                    <input
+                        type="text"
+                        placeholder="read a book"
+                        className="shadow p-3 mx-4"
+                        value={inputValue}
+                        onChange={e => setInputValue(e.target.value)}
+                    />
+                    <button
+                        className="mb-10 px-6 py-2  shadow-lg bg-gray-50"
+                        onClick={() => {
+                            dispatch({ type: 'ADD-TASK', payload: inputValue })
+                            setInputValue('')
+                        }}
+                    >Add Task</button>
+                    {/* {state.loading ? 'Loading' : JSON.stringify(state, null, 2)} */}
+                    {console.log('current state: ', state)}
+                    {state.loading ? 'Loading' : <TaskList items={state.taksList} />}
+                </div>
+            </div>
+        </Context.Provider>
+    );
+}
+
+export default Home;
+
+const TaskList = ({ items }) => {
+    return items.map(item => <Task key={item.id} context={Context} {...item} />)
+
+}
+
